582be0fcf4be63175fa4d514f61061ff9ba795a2
Neiklneev <neikln33v@gmail.com>
2024-10-19 12:39:25 +0400

    Update leap.lua

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,6 +2,6 @@ return {
   "ggandor/leap.nvim",
   config = function()
     vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-    vim.keymap.set({'n', 'x', 'o'}, 't', '<Plug>(leap-backward)')
+    vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
   end
 }
