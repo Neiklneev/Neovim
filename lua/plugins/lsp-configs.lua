73f8992f2d4ff623791fb3672d25971d1ea3ff9b
Atharv Agrawal <neikln33v@gmail.com>
2024-11-05 19:40:28 +0400

    Added auto install for several linters

--- lua/plugins/lsp-configs.lua
+++ lua/plugins/lsp-configs.lua
@@ -9,10 +9,13 @@ return {
       "williamboman/mason-lspconfig.nvim",
       config = function()
         require("mason-lspconfig").setup({
-          ensure_installed={
+          ensure_installed = {
             "lua_ls",
             "pyright",
             "jdtls",
+            "isort",
+            "black",
+            "stylua"
           }
         })
       end
@@ -32,11 +35,10 @@ return {
       })
       lspconfig.jdtls.setup({
         capabilities = capabilities,
-})
+      })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', 'K',  vim.lsp.buf.hover, {})
-
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end
   }
 }
